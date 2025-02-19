7d7731be23ea198a4fd0ad00c7a8881578aab46b
Vintral <jeffrey.heater@gmail.com>
2025-02-19 04:22:47 -0700

    Rework GUID handling for RountTechnology

--- models/round_technology.go
+++ models/round_technology.go
@@ -2,14 +2,20 @@ package models
 
 import (
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type RoundTechnology struct {
 	BaseModel
 
-	GUID         uuid.UUID          `gorm:"uniqueIndex,size:36" json:"guid"`
+	GUID         uuid.UUID          `gorm:"uniqueIndex,size:36" json:"-"`
 	RoundID      uint               `json:"-"`
 	TechnologyID uint               `json:"-"`
 	Available    bool               `json:"-"`
 	Levels       []*TechnologyLevel `gorm:"-"`
+}
+
+func (technology *RoundTechnology) BeforeCreate(tx *gorm.DB) (err error) {
+	technology.GUID = uuid.New()
+	return
 }
