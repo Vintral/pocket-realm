61c634d206c1be7d3c056aa011d551ac275a8440
Vintral <jeffrey.heater@gmail.com>
2025-02-19 04:26:19 -0700

    Update Technology model
    - Added LoadForUser method for looking up current level and cost for next one for a given user
    - Added data to Dump method
    - Add looking up technology ID for a given guid value

--- models/technology.go
+++ models/technology.go
@@ -1,21 +1,26 @@
 package models
 
 import (
+	"context"
 	"fmt"
+	"sync"
 
 	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
+	"go.opentelemetry.io/otel/attribute"
 	"gorm.io/gorm"
 )
 
 type Technology struct {
 	BaseModel
 
-	GUID      uuid.UUID `gorm:"uniqueIndex,size:36"`
-	Name      string
-	Buff      uint
-	Available bool               `gorm:"->;-:migration" json:"available"`
-	Levels    []*TechnologyLevel `gorm:"-"`
+	GUID      uuid.UUID          `gorm:"uniqueIndex,size:36" json:"guid"`
+	Name      string             `json:"name"`
+	Buff      uint               `json:"-"`
+	Available bool               `gorm:"->;-:migration" json:"-"`
+	Levels    []*TechnologyLevel `gorm:"-" json:"-"`
+	Level     uint               `gorm:"-" json:"level"`
+	Cost      uint               `gorm:"-" json:"cost"`
 }
 
 func (technology *Technology) BeforeCreate(tx *gorm.DB) (err error) {
@@ -36,14 +41,58 @@ func (technology *Technology) AfterFind(tx *gorm.DB) (err error) {
 	return
 }
 
+func (technology *Technology) LoadForUser(baseContext context.Context, wg *sync.WaitGroup, user *User, tech *Technology) {
+	ctx, sp := Tracer.Start(baseContext, "technology.LoadForUser")
+	defer sp.End()
+
+	if wg != nil {
+		defer wg.Done()
+	}
+
+	tech.ID = technology.ID
+	tech.GUID = technology.GUID
+	tech.Name = technology.Name
+
+	log.Trace().Any("guid", tech.GUID).Msg("LoadForUser")
+
+	sp.SetAttributes(
+		attribute.Int("user", int(user.ID)),
+		attribute.Int("round", user.RoundID),
+		attribute.Int("technology", int(technology.ID)),
+	)
+
+	db.WithContext(ctx).Table("user_technologies").Select("level").Where("user_id = ? AND round_id = ? AND technology_id = ?", user.ID, user.RoundID, technology.ID).Scan(&tech.Level)
+
+	for i := 0; i < len(technology.Levels); i++ {
+		if technology.Levels[i].Level == tech.Level+1 {
+			tech.Cost = technology.Levels[i].Cost
+			return
+		}
+	}
+}
+
 func (technology *Technology) Dump() {
 	log.Trace().Msg("==================================")
+	log.Trace().Msg("ID: " + fmt.Sprint(technology.ID))
 	log.Trace().Msg("Name: " + technology.Name)
 	log.Trace().Msg("Buff: " + fmt.Sprint(technology.Buff))
 	log.Trace().Msg("Available: " + fmt.Sprint(technology.Available))
+	log.Trace().Msg("Player Level: " + fmt.Sprint(technology.Level))
 	log.Trace().Msg("--------------LEVELS--------------")
 	for _, level := range technology.Levels {
 		log.Trace().Msg(fmt.Sprint(level.Level) + " -- " + fmt.Sprint(level.Cost))
 	}
 	log.Trace().Msg("==================================")
+}
+
+func GetTechnologyIdForGuid(baseContext context.Context, tech uuid.UUID) uint {
+	ctx, sp := Tracer.Start(baseContext, "technology.GetTechnologyIdForGuid")
+	defer sp.End()
+
+	log.Info().Any("guid", tech).Msg("LoadTechnologyByGUID")
+
+	var technology *uint
+	db.WithContext(ctx).Table("round_technologies").Where("guid = ?", tech).Select("technology_id").Scan(&technology)
+
+	return *technology
 }
